e1a5afb57dce449a70fbeda8ecddc191d4c2469c
mazh1993 <114928981+mazh1993@users.noreply.github.com>
2024-09-01 22:42:34 +0800

    proposedModel.R

--- proposedModel.R
+++ proposedModel.R
@@ -1,3 +1,44 @@
+library(nimble)
+  library(coda)
+  library(MASS)
+  library(sp)
+  library(fossil)
+  library(purrr)
+  library(parallel)
+  library(foreach)
+  library(doParallel)
+  library(DescTools)
+  set.seed(1000)
+  load("DATA.Rdata")
+  
+  load("china_adj.RData")
+  colnames <- china_adj$chinapoly[[6]]
+  
+  adj_matrix <- china_adj$adj[-c(9,28,29),-c(9,28,29)]
+  str(adj_matrix)
+  rowSums(adj_matrix)
+  as.vector(colSums(adj_matrix))
+  
+  library(Matrix)
+  row_adj <- Matrix::Diagonal(x = 1 / sqrt(Matrix::rowSums(adj_matrix^2))) %*% adj_matrix
+  
+  unitind <- function(x) {
+    #return(which(x == "1"))
+    return(which(x != 0))
+  }
+  
+  adj1 <- as.vector(apply(row_adj, 1, unitind))
+  adjvector <- c()
+  adjnum <- c()
+  for (i in 1:28) {
+    adjvector <- append(adjvector, as.vector(adj1[[i]]))
+    adjnum[i] <- length(as.vector(adj1[[i]]))
+  }
+  
+  eigenadj <- eigen(adj_matrix)$values
+  maxeigenadj <- max(eigenadj); maxeigenadj
+  mineigenadj <- min(eigenadj); mineigenadj
+
 ##### proposed model ######
 NigCode <- nimbleCode({
   for (i in 1:N) {
@@ -84,6 +125,51 @@ NigCode <- nimbleCode({
   
   xm_s ~ T(dnorm(1, 0.1),0,)
 })
+
+
+R <- (is.na(DATA[[k]]$x)+0)   # kth replicate
+    NigData <- list(y = DATA[[k]]$y, z1 = DATA[[k]]$z1, z2 = DATA[[k]]$z2, 
+                    z3 = DATA[[k]]$z3, z4 = DATA[[k]]$z4, z5 = DATA[[k]]$z5, 
+                    x = DATA[[k]]$x, medx = DATA[[k]]$xm,
+                    alpha_pclu = c(1/3,1/3,1/3), 
+                    R = R, delta_g2 = rep(0,28),
+                    mine = 1/mineigenadj,
+                    maxe = 1/maxeigenadj, I = diag(28), A = adj_matrix,
+                    mu_wy = rep(0,28), mu_wr=rep(0,28))
+    NigConsts <- list(N = length(NigData$y),  
+                      prov = DATA[[k]]$prov, L = 124)
+    x_init <- ifelse(is.na(DATA[[k]]$x),1,NA)
+    eta_init <- matrix(rep(c(0,1,2,3), NigConsts$N), nrow = NigConsts$N, ncol = 4, byrow = T)
+    NigInits <- list(beta = rnorm(6), alpha = rnorm(6),
+                     gamma = rnorm(10), betax = rnorm(28),
+                     sigma_wy = 1, sigma_wr = 1, 
+                     Wy = rep(1, 28), Wr = rep(1, 28),
+                     x = x_init, tau_x = 1, xm_s = 1,
+                     mu_R = runif(NigConsts$N), eta = eta_init, 
+                     gammax0 = rnorm(28), mu_x = runif(NigConsts$N), 
+                     mu_g1 = -1, mu_g3 = 1, tau_g1 = 1, tau_g3 = 1, 
+                     delta_g1 = runif(28, -1,0), delta_g3 = runif(28, 0,1),
+                     pclu = c(0.1,0.3,0.6), phi = matrix(0, nrow = 4, ncol = 6),
+                     clu = c(rep(1,10), rep(2,10), rep(3, 8)),
+                     gamma_wy = 0, gamma_wr = 0,
+                     L_wy = diag(28), L_wr = diag(28),
+                     py = matrix(rep(c(0,0.1,0.2,0.3,0.4),NigConsts$N), nrow=NigConsts$N, ncol = 5, byrow = T)
+    )
+    Nig <- nimbleModel(code = NigCode, name = "Nig", constants = NigConsts,
+                       data = NigData, inits = NigInits)
+    cNig <- compileNimble(Nig)
+    Nigconf <- configureMCMC(Nig, print = FALSE)
+    Nigconf$addMonitors(c("beta","alpha","gamma", "gammax0",
+                          "tau_x","phi",
+                          "sigma_wy","sigma_wr",
+                          "gamma_wy", "gamma_wr","clu", 
+                          "xm_s"
+    ))
+    Nigmcmc <- buildMCMC(Nigconf)
+    cNigmcmc <- compileNimble(Nigmcmc, project = Nig)
+    cNig$setInits(NigInits)
+    mcmc.out <- runMCMC(cNigmcmc, niter = 150000, setSeed = 123, thin = 15)
+    pos_mcmc <- as.mcmc(mcmc.out[-c(1:4000),])
 
 
 
